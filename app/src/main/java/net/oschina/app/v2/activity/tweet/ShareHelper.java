89dc62168c8caa5fbb7e6ede52358469b87b81a0
Yue.Wu <yue.wu@zhan.com>
2016-05-29 20:52:22 +0800

    还原

--- app/src/main/java/net/oschina/app/v2/activity/tweet/ShareHelper.java
+++ app/src/main/java/net/oschina/app/v2/activity/tweet/ShareHelper.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.shiyanzhushou.app.R;

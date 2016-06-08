13b5e548163cefd610bd34b6b0b4892974a0d919
Yue.Wu <yue.wu@zhan.com>
2016-06-08 12:56:14 +0800

    粉丝增加筛选

--- app/src/main/java/net/oschina/app/v2/activity/user/fragment/FunsForHelpFragment.java
+++ app/src/main/java/net/oschina/app/v2/activity/user/fragment/FunsForHelpFragment.java
@@ -11,19 +11,79 @@ import net.oschina.app.v2.base.ListBaseAdapter;
 import net.oschina.app.v2.model.Ask;
 import net.oschina.app.v2.model.AskList;
 import net.oschina.app.v2.model.ListEntity;
+import net.oschina.app.v2.model.event.FansTabEvent;
 import net.oschina.app.v2.utils.UIHelper;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.shiyanzhushou.app.R;
+
+import de.greenrobot.event.EventBus;
 
 public class FunsForHelpFragment extends BaseListFragment {
 	protected static final String TAG = FunsForHelpFragment.class
 			.getSimpleName();
 	private static final String CACHE_KEY_PREFIX_10 = "funsforhelplist_";
+	private int mStatus =1;//1 全部  2 已回答 3 未回答
+
+	//Tab fans views
+	private TextView mViewAllQuestion;
+	private TextView mViewAnsweredQuestion;
+	private TextView mViewUnansweredQuestion;
+
+	@Override
+	protected void initViews(View view) {
+		super.initViews(view);
+
+		mViewAllQuestion=(TextView)view.findViewById(R.id.all_question);
+		mViewAnsweredQuestion=(TextView)view.findViewById(R.id.answered_question);
+		mViewUnansweredQuestion=(TextView)view.findViewById(R.id.unanswered_question);
+		mViewAllQuestion.setOnClickListener(mFansTabClickListener);
+		mViewAnsweredQuestion.setOnClickListener(mFansTabClickListener);
+		mViewUnansweredQuestion.setOnClickListener(mFansTabClickListener);
+	}
+
+	private View.OnClickListener mFansTabClickListener=new View.OnClickListener(){
+		@Override
+		public void onClick(View v) {
+			mViewAllQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+			mViewAllQuestion.setBackgroundResource(R.drawable.default_bg);
+			mViewAnsweredQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+			mViewAnsweredQuestion.setBackgroundResource(R.drawable.default_bg);
+			mViewUnansweredQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+			mViewUnansweredQuestion.setBackgroundResource(R.drawable.default_bg);
+
+			switch (v.getId()){
+				case R.id.all_question:
+					mViewAllQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+					mViewAllQuestion.setBackgroundResource(R.drawable.bg_blue_underline);
+					mStatus=1;
+					break;
+				case R.id.answered_question:
+					mViewAnsweredQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+					mViewAnsweredQuestion.setBackgroundResource(R.drawable.bg_blue_underline);
+					mStatus=3;
+					break;
+				case R.id.unanswered_question:
+					mViewUnansweredQuestion.setTextColor(getResources().getColor(R.color.text_dark));
+					mViewUnansweredQuestion.setBackgroundResource(R.drawable.bg_blue_underline);
+					mStatus=2;
+					break;
+			}
+			setRefresh();
+		}
+	};
 
 	@Override
 	protected ListBaseAdapter getListAdapter() {
 		return new funsForHelperAdapter(getActivity());
 		//return new FunsForHelpAdapter();
+	}
+
+	@Override
+	protected int getLayoutRes() {
+		return R.layout.my_fans_fragment_pull_refresh_listview;
 	}
 
 	// 缓存前缀
@@ -52,8 +112,12 @@ public class FunsForHelpFragment extends BaseListFragment {
 	protected void sendRequestData() {
 //		int uid=AppContext.instance().getLoginUid();
 //		NewsApi.getFunsForHelpList(uid, mCurrentPage, mJsonHandler);
+		//Old changed by wuyue
+		/*mCurrentPage=mCurrentPage==0 ? 1 : mCurrentPage;
+		NewsApi.getFunsForHelpList(AppContext.instance().getLoginUid(), mCurrentPage, mJsonHandler);*/
+
 		mCurrentPage=mCurrentPage==0 ? 1 : mCurrentPage;
-		NewsApi.getFunsForHelpList(AppContext.instance().getLoginUid(), mCurrentPage, mJsonHandler);
+		NewsApi.getFansAskList(AppContext.instance().getLoginUid(), mCurrentPage, mStatus, mJsonHandler);
 	}
 
 	@Override
